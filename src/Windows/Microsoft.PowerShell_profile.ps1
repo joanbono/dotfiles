4fe7a6f3871c93054d92d179b9459b7ac6fda849
Bono <joan@bono.cat>
2018-01-15 11:53:33 +0100

    Added Windows dotfiles

---
+++ src/Windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+﻿#Disable Bell at backspace
+Set-PSReadlineOption -BellStyle None
+#Set-Service beep -StartupType disabled
+
+#Set Prompt
+function prompt {
+    Write-Host ("jbono@EliteBook ") -NoNewLine
+    Write-Host ("[" + $( Split-Path -Path $(Get-Location) -Leaf -Resolve ) +"]>") -NoNewLine -Foreground "yellow"
+    return " "
+}
+
+#Alias
+set-alias -name gitshell -value C:\Tools\gitshell.ps1
